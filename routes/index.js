2c38a36677c565b705ddd8965c27aa092a28a627
Baley Wilson <baleywilson@u.boisestate.edu>
2022-11-21 17:48:00 -0700

    add and remove working

--- routes/index.js
+++ routes/index.js
@@ -16,13 +16,14 @@ router.get('/', function (req, res, next) {
         (err, rows) => {
           if (rows.length === 1) {
             console.log("Table exists!");
-            db.all(` select blog_username , blog_txt from posts`, (err, rows) => {
+            db.all(` select blog_id, blog_username , blog_txt from posts`, (err, rows) => {
               //console.log("returning " + rows.length + " records");
               res.render('index', { title: 'Baleys blog', data: rows });
             });
           } else {
             console.log("Table does not exist, inserting data");
             db.exec(`create table posts (
+                     blog_id INTEGER PRIMARY KEY AUTOINCREMENT,
                      blog_username text NOT NULL UNIQUE,
                      blog_txt text NOT NULL);
                       insert into posts (blog_username, blog_txt)
@@ -57,5 +58,21 @@ router.post('/add', (req, res, next) => {
       res.redirect('/');
     }
   );
+})
+
+router.post('/delete', (req, res, next) => {
+  console.log("Delete working");
+  var db = new sqlite3.Database('mydb.sqlite3',
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+      if (err){
+        console.log("Getting error " + err);
+        exit(1);
+      }
+      db.exec(`delete from posts where blog_id='${req.body.blogid}';`);
+      res.redirect('/');
+    }
+  )
+
 })
 module.exports = router;
